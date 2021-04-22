3da35982f7d0bbe293c64ab54b80b5ae3e56208c
psingh86 <prabhasis.singh70@gmail.com>
2021-04-21 22:43:51 -0500

    user table created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_22_033905) do
+ActiveRecord::Schema.define(version: 2021_04_22_034310) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -35,6 +35,15 @@ ActiveRecord::Schema.define(version: 2021_04_22_033905) do
     t.float "price"
     t.boolean "isSale"
     t.string "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "password"
+    t.string "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
